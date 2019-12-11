ff383f3e2b2af5bdd8bdb2d3a1e5072273d393a1
hagarnegm <h.negm1997@gmail.com>
2019-12-11 16:54:09 +0200

    initial commit

---
+++ identification.py
@@ -0,0 +1,26 @@
+from commonfunctions import *
+from preprocessing import *
+import numpy as np
+import cv2
+import skimage.io as io
+from scipy import ndimage
+from numpy import unravel_index
+
+def refpoint(img):
+    dist_trans = ndimage.distance_transform_edt(img)
+    centroid = unravel_index(dist_trans.argmax(), dist_trans.shape)
+    x = centroid[0]
+    y = centroid[1]
+    img_centroid = np.copy(img)
+    cv2.circle(img_centroid, (x, y), 80, (0, 0, 0), -1)
+    return dist_trans, img_centroid
+
+# def fingeridentification(img):
+
+
+rgb_img = io.imread("trial.jpg")
+cr_img = rgb2cr(rgb_img)
+segmented = segment(cr_img)
+dist_trans, img_centroid = refpoint(segmented)
+
+show_images([rgb_img, cr_img, segmented, dist_trans, img_centroid])
--- preprocessing.py
+++ preprocessing.py
@@ -1,2 +1,23 @@
-import numpy
+from commonfunctions import *
+import numpy as np
+import skimage.io as io
+
+
+def rgb2cr(rgb_img):
+    cr_img = np.zeros((rgb_img.shape[0], rgb_img.shape[1]))
+    r = rgb_img[:, :, 0]
+    g = rgb_img[:, :, 1]
+    b = rgb_img[:, :, 2]
+    cr_img = 0.5*r - 0.418688*g - 0.081312*b
+
+    return cr_img
+
+
+def segment(cr_img):
+    segmented = np.copy(cr_img)
+    segmented[segmented[:, :] < 0] = 0
+    segmented[segmented[:, :] > 0] = 1
+
+    return segmented
+
 
