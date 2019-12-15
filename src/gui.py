629e259242e19794b76d9b2d4c442fa3d76d6694
hagarnegm <h.negm1997@gmail.com>
2019-12-15 18:15:25 +0200

    .

--- src/gui.py
+++ src/gui.py
@@ -1,53 +1,81 @@
-import tkinter as tk
+import tkinter as tk, threading
 import cv2
+import matplotlib.pyplot as plt
 from PIL import Image, ImageTk
 from tkinter import filedialog
 
 
 def stream():
+    thread = threading.Thread(target=video, args=())
+    thread.daemon = 1
+    thread.start()
+
+
+def video():
     global display1
     global display2
     global images
-    width, height = 800, 600
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-    ret, frame = cap.read()
-    image = cv2.flip(frame, 1)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
-    image = Image.fromarray(image)
-    image = ImageTk.PhotoImage(image)
-    images.append(frame)
 
     if display2 is not None:
         display2.destroy()
 
-    if display1 is None:
-        display1 = tk.Label(root)
-        display1.config(bd='5')
-        display1.place(x=600, y=150)
-        display1.image = image
-        display1.configure(image=image)
-        display1.after(10, stream)
-    else:
-        display1.image = image
-        display1.configure(image=image)
-        display1.after(10, stream)
+    vc = cv2.VideoCapture(0)
 
-    cap.release()
+    plt.ion()
+
+    if vc.isOpened():  # try to get the first frame
+        is_capturing, frame = vc.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # makes the blues image look real colored
+        # bg = remove_g_noise(frame)
+        webcam_preview = plt.imshow(frame)
+
+    else:
+        is_capturing = False
+    fig = plt.figure(figsize=(50, 50))
+    columns = 10
+    rows = 10
+    for i in range(1, columns*rows +1):
+        try:  # Lookout for a keyboardInterrupt to stop the script
+            is_capturing, frame = vc.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # makes the blues image look real colored
+            # webcam_preview.set_data(frame)
+            # webcam_preview = plt.imshow(frame)
+            fig.add_subplot(rows, columns, i)
+            plt.imshow(frame)
+            images.append(frame)
+
+        except KeyboardInterrupt:
+            vc.release()
+    plt.show()
     # time = 1000
     # while cap.isOpened():
-    #     cap.set(cv2.CAP_PROP_POS_MSEC, time)
-    #     images.append(frame)
+    #     # cap.set(cv2.CAP_PROP_POS_MSEC, time)
+    #     ret, frame = cap.read()
+    #     image = cv2.flip(frame, 1)
+    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
+    #     image = Image.fromarray(image)
+    #     image = ImageTk.PhotoImage(image)
+    #     # images.append(frame)
+    #
+    #     if display1 is None:
+    #         display1 = tk.Label(root)
+    #         display1.config(bd='5')
+    #         display1.place(x=400, y=200)
+    #         display1.image = image
+    #         display1.configure(image=image)
+    #         display1.after(10, video)
+    #     else:
+    #         display1.image = image
+    #         display1.configure(image=image)
+    #         display1.after(10, video)
+    #
     #     if not ret:
     #         break
-    #     time += 1000
+        # time += 1000
+    # cap.release()
 
 
-
-
-def upload():
+def photo():
     global display1
     global display2
     path = filedialog.askopenfilename()
@@ -62,7 +90,7 @@ def upload():
         if display2 is None:
             display2 = tk.Label(image=image)
             display2.image = image
-            display2.place(x=600, y=150)
+            display2.place(x=600, y=200)
         else:
             display2.configure(image=image)
             display2.image = image
@@ -71,6 +99,7 @@ def upload():
 class GUI:
 
     def __init__(self, master):
+        global output
         self.master = master
         master.title('Sign Language Recognition')
         master.configure(background='#40407a')
@@ -80,25 +109,26 @@ class GUI:
         self.title = tk.Label(root, text='Sign Language Recognition', font='Helvetica', bg='#40407a', fg='white')
         self.title.place(x=650, y=50)
 
-        self.stream_video = tk.Button(root, bg='white', text='Stream', command=stream)
+        self.stream_video = tk.Button(root, bg='white', text='Video', command=stream)
         self.stream_video.config(width=50)
-        self.stream_video.place(x=50, y=150)
+        self.stream_video.place(x=50, y=100)
 
-        self.upload_image = tk.Button(root, bg='white', text='Upload', command=upload)
+        self.upload_image = tk.Button(root, bg='white', text='Photo', command=photo)
         self.upload_image.config(width=50)
-        self.upload_image.place(x=50, y=200)
+        self.upload_image.place(x=600, y=100)
 
-        self.process_image = tk.Button(root, bg='white', text='Process', command=upload)
+        self.process_image = tk.Button(root, bg='white', text='Process', command=photo)
         self.process_image.config(width=50)
-        self.process_image.place(x=50, y=250)
+        self.process_image.place(x=1120, y=100)
 
         self.output = tk.Label(root, text='Output Text: ', font='Helvetica', bg='#40407a', fg='white')
-        self.output.place(x=600, y=700)
+        self.output.place(x=50, y=700)
 
 
 root = tk.Tk()
 display1 = None
 display2 = None
+text = None
 images = []
 gui = GUI(root)
 root.mainloop()
